dea041142ce9579ac75241fdb7ae2f1df3c662d7
NavierX90 <n57751052@gmail.com>
2025-10-09 13:43:07 +0300

    Add files via upload

---
+++ numbers_descending.c
@@ -0,0 +1,18 @@
+//descending numbers 100 to 50 
+/* Name:Dennis mwangi kuria 
+Reg: CT101/G/26506/25 
+Description: A prorgam of dscending numbers from 100 to 50 
+*/
+//the header
+#include <stdio.h> 
+
+int main(){
+	int i;
+	//use a for loop
+	for (i=100;i>=50;i--)
+	{
+		printf("%d\n",i); //\n make each number to be printed in a nes line
+		
+	}
+	return 0;
+}
